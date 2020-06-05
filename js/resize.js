50b0b191feea6fa086a0348f33956e10e888515b
Corey Emery <cemery@andrew.cmu.edu>
2020-06-05 15:39:32 -0400

    RESIZING WORKS

--- js/resize.js
+++ js/resize.js
@@ -1,3 +1,6 @@
+// Code copied from CSS-tricks to help with mobile full-height screens (working around menu bars)
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
