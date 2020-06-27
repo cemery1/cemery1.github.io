2596b582fef0534fe4cc4b788f8db6ee0ff24ead
Corey Emery <cemery@andrew.cmu.edu>
2020-06-27 14:19:36 -0400

    update section content (without templates), new mobile navbar (projects tab)

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,12 @@
-// Some code inspired by Ari Daly Portfolio
+// Some scroll-related code inspired by Ari Daly Portfolio
 // https://github.com/acdaly/acdaly.github.io
 
 $(document).ready(function() {
+    // Determine initial state of navbar
+    (window.innerWidth < 576)
+        ? ($("#design-link, #software-link, #data-link").hide())
+        : ($("#project-link").hide());
+
     // Smooth scrolling between sections
     $(".nav-link, .navbar-brand").on("click", function(e) {
         // Prevent id from being added to the URL (unless it's the resume link)
@@ -13,7 +18,19 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop: $(destination).offset().top - navbarOffset}, "slow");
     });
 
-    // Update navbar when moving between sections
+    // Update navbar based on the width of the screen
+    $(window).resize(function() {
+        // Get current screen position and offset
+        if (window.innerWidth < 576) {
+            $("#design-link, #software-link, #data-link").hide();
+            $("#project-link").show();
+        } else {
+            $("#design-link, #software-link, #data-link").show();
+            $("#project-link").hide()
+        }
+    });
+
+    // Update navbar highlight when moving between sections
     $(document).scroll(function() {
         // Get current screen position and offset
         var curPos = $(document).scrollTop();
@@ -22,15 +39,18 @@ $(document).ready(function() {
         if (curPos > $("#about").offset().top - offset) { // in the about section
             $(".active").removeClass("active");
             $("#about-link").addClass("active");
-        /* } else if (curPos > $("#data").offset().top - offset) { // in the data science section
+        } else if (curPos > $("#data").offset().top - offset) { // in the data science section
             $(".active").removeClass("active");
             $("#data-link").addClass("active");
+            $("#project-link").addClass("active");
         } else if (curPos > $("#software").offset().top - offset) { // in the software section
             $(".active").removeClass("active");
             $("#software-link").addClass("active");
+            $("#project-link").addClass("active");
         } else if (curPos > $("#design").offset().top - offset) { // in the design and user research section
             $(".active").removeClass("active");
-            $("#design-link").addClass("active"); */
+            $("#design-link").addClass("active");
+            $("#project-link").addClass("active");
         } else { // on the home page
             $(".active").removeClass("active");
         }
